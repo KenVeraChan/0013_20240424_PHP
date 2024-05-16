83d5a6fb3fb5e1b42177c57046d4098a0d824755
KenVeraChan <kenveracha@gmail.com>
2024-05-16 11:15:34 +0200

    Ficheros finalizados antes del CRUD

--- 0018-ProyectoPHP/TratamientoRegistros.php
+++ 0018-ProyectoPHP/TratamientoRegistros.php
@@ -25,7 +25,7 @@
         }
         if($actualizando!=NULL)
         {
-           // actualizarDatos();
+           actualizarDatos();
         }
         // 1) INSERTAR ALGUN NUEVO REGISTRO A LA BBDD
         function insertarDatos()
@@ -170,7 +170,72 @@
                         mysqli_select_db($conexion,$db_nombre);
                         // CODIGO EN EL CASO QUE FUNCIONE EL RECONOCIMIENTO CON LA BBDD//
                         //Numeros sin comillas
+                        echo "<br><br>";
                         $consulta="SELECT * FROM CLIENTES WHERE CÓDIGOCLIENTE='$Codigo_Cliente'";
+                        echo "$consulta<br><br>";
+                        $resultados=mysqli_query($conexion,$consulta);
+                        //CODIGO DE INSERCCION DE DATOS EN LA BBDD DE PHP MYADMIN
+                            if($resultados==false)
+                            {
+                                echo "Error en la consulta";
+                            }
+                            else{
+                                //COMPRUEBA SI SE LE DIO AL BOTON DE REGISTRAR SIN CAMPOS RELLENADOS
+                                if(strcmp($Codigo_Cliente,"")!=0)
+                                {
+                                    //SI TODAVIA NO SE HA BORRADO EL REGISTRO PEDIDO
+                                    echo "<br>Datos del registro consultado: ".$Codigo_Cliente;
+                                    echo "<br>-------------------------------------<br>";
+                                    // SI SALE BIEN, SE SACARA POR PANTALLA LOS DATOS INTRODUCIDOS
+                                    while($fila=mysqli_fetch_array($resultados,MYSQLI_ASSOC))
+                                    {
+                                        echo "<table><tr><td>";
+                                        echo $fila['CÓDIGOCLIENTE']. "</td><td>";
+                                        echo $fila['EMPRESA']. "</td><td>";
+                                        echo $fila['DIRECCIÓN']. "</td><td>";
+                                        echo $fila['TELÉFONO']. "</td><td>";
+                                        echo $fila['POBLACIÓN']. "</td><td>";
+                                        echo $fila['RESPONSABLE']. "</td><td>";
+                                        echo $fila['HISTORIAL']. "</td></tr></table>";
+                                        echo "<br><br>";    
+                                    }  
+                                }
+                                else
+                                {
+                                    echo "No hay registros existentes con el código pedido o ya se había borrado antes!";
+                                }
+                            }
+                        mysqli_close($conexion);
+                    /////MANEJO DE ERRORES ////
+                    }catch(mysqli_sql_exception $error2){
+                        //se podria crear pagina web de mostrar error
+                        echo"Base de datos NO RECONOCIDA";
+                        exit();
+                    }
+            } catch (mysqli_sql_exception $error) {
+                //se podria crear pagina web de mostrar error
+                echo "Fallo al conectar con la BBDD";
+                exit(); //Sale del codigo de PHP
+            }   
+        }
+        // 4) CAMBIA ALGUN DATO EXISTENTE DE LOS QUE SE LE PIDA
+        function actualizarDatos()
+        {
+            require("conexionPHP-5.php");
+            $Codigo_Cliente=$_GET["COD"];
+            //Se hace llamada al fichero de conexion PHP
+            //Se podria haber hecho con INCLUDE
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    
+            try {
+                $conexion=mysqli_connect($db_host,$db_usuario,$db_contrasenia); 
+                mysqli_set_charset($conexion,"utf8");
+                // CODIGO EN EL CASO QUE FUNCIONE LA CONEXION //
+                    try{
+                        mysqli_select_db($conexion,$db_nombre);
+                        // CODIGO EN EL CASO QUE FUNCIONE EL RECONOCIMIENTO CON LA BBDD//
+                        //Numeros sin comillas
+                        $consulta="UPDATE CLIENTES set DIRECCIÓN='AMADEO ARIAS 40' where CÓDIGOCLIENTE='$Codigo_Cliente";
                         $resultados=mysqli_query($conexion,$consulta);
                         //CODIGO DE INSERCCION DE DATOS EN LA BBDD DE PHP MYADMIN
                             if($resultados==false)
