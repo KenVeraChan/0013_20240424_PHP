58d3898c14bed2e8a21c000e7e80f0d8928cc571
KenVeraChan <kenveracha@gmail.com>
2024-07-30 21:51:43 +0200

    Agregado un apunte sin mejoras

--- 0038-ProyectoPHP/MODELO/lecturaImagen.php
+++ 0038-ProyectoPHP/MODELO/lecturaImagen.php
@@ -9,9 +9,10 @@
     $contenidoFich="";
     $tipoFich="";
 
-    $consulta=$conexion->query("SELECT * FROM $BD_tabla WHERE id=18");
-    $resultado=$consulta->fetchAll(PDO::FETCH_ASSOC);
-    foreach($resultado as $fila):
+    $sql="SELECT * FROM $BD_tabla WHERE id=?";
+    $consulta=$conexion->prepare($sql);
+    $consulta->execute(array("21"));
+    while($fila=$consulta->fetch(PDO::FETCH_ASSOC)):
     {
         $idFich=$fila["id"];
         $nombreFich=$fila["Nombre"];
@@ -22,10 +23,11 @@
         <div>Nombre: <?php echo $nombreFich;?></div><br>
         <div>Tipo: <?php echo $tipoFich;?></div><br>
         <div style="border: solid; color: green">
-            <img src="data:image/jpeg;base64,<?php echo base64_encode($contenidoFich);?>" 
+            <img src="data:image/png;base64,<?php echo base64_encode($contenidoFich);?>" 
                 alt="Esta es la primera imagen" 
-                style="width: 60%">
+                style="width: 70%">
         </div>
     <?php
-        }endforeach;               
+        }endwhile; 
+        $consulta->closeCursor();             
     ?>
